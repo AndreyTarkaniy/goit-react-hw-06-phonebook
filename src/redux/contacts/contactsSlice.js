d2fdd127b98b47e0d2a78a042620d45dd3aa24fc
AndreyTarkaniy <endrubass@gmail.com>
2023-06-17 23:14:23 +0300

    add redux-persist

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,8 +1,10 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import persistReducer from 'redux-persist/es/persistReducer';
+import storage from 'redux-persist/lib/storage';
 
 import ContactsData from 'components/data/contacts.json';
 
-export const contactsSlice = createSlice({
+const contactsSlice = createSlice({
   name: 'contatcs',
   initialState: { contacts: ContactsData, filter: '' },
   reducers: {
@@ -23,5 +25,12 @@ export const contactsSlice = createSlice({
   },
 });
 
+const persistContactsSlice = persistReducer(
+  { key: 'contacts', storage, whitelist: ['contacts'] },
+  contactsSlice.reducer
+);
+
 export const { addContact, deleteContact, changeFilter } =
   contactsSlice.actions;
+
+export default persistContactsSlice;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-import { contactsSlice } from './contacts/contactsSlice';
+import contactsReducer from './contacts/contactsSlice';
+
+// contactsReducer - те саме persistContactsSlice
 
 export const store = configureStore({
-  reducer: { contacts: contactsSlice.reducer },
+  reducer: { contacts: contactsReducer },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
